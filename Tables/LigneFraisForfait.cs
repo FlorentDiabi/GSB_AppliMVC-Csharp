122eec1198618e60223256a1df665c4b11dfb7e7
FlorentDiabi <diabi.florent@gmail.com>
2021-11-10 15:11:34 +0100

    Création des dernières classes manquantes

---
+++ Tables/LigneFraisForfait.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_AppliMVC.Tables
+{
+    class LigneFraisForfait
+    {
+        private string idVisiteur;
+        private string mois;
+        private string idFraisForfait;
+        private int quantite;
+
+        public LigneFraisForfait(string idVisiteur, string mois, string idFraisForfait, int quantite)
+        {
+            this.idVisiteur = idVisiteur;
+            this.mois = mois;
+            this.idFraisForfait = idFraisForfait;
+            this.quantite = quantite;
+        }
+
+        public string IdVisiteur { get => idVisiteur; set => idVisiteur = value; }
+        public string Mois { get => mois; set => mois = value; }
+        public string IdFraisForfait { get => idFraisForfait; set => idFraisForfait = value; }
+        public int Quantite { get => quantite; set => quantite = value; }
+    }
+}
---
+++ Tables/PuissanceVehicule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_AppliMVC.Tables
+{
+    class PuissanceVehicule
+    {
+        private int id;
+        private string puissance;
+        private string typeVehicule;
+        private decimal prixKm;
+
+        public PuissanceVehicule(int id, string puissance, string typeVehicule, decimal prixKm)
+        {
+            this.id = id;
+            this.puissance = puissance;
+            this.typeVehicule = typeVehicule;
+            this.prixKm = prixKm;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Puissance { get => puissance; set => puissance = value; }
+        public string TypeVehicule { get => typeVehicule; set => typeVehicule = value; }
+        public decimal PrixKm { get => prixKm; set => prixKm = value; }
+    }
+}
