5039f7ff9e49b18eabbf7ca7ae0dce0ca13ad053
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2022-01-09 00:03:20 +0100

    ORM EntityFramework

---
+++ Gsb/Puissancevehicule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace GSB_AppliMVC_Csharp.Gsb
+{
+    public partial class Puissancevehicule
+    {
+        public Puissancevehicule()
+        {
+            Lignepuissancevehicule = new HashSet<Lignepuissancevehicule>();
+        }
+
+        public int Id { get; set; }
+        public string Puissance { get; set; }
+        public string Typevehicule { get; set; }
+        public decimal Prixkm { get; set; }
+
+        public virtual ICollection<Lignepuissancevehicule> Lignepuissancevehicule { get; set; }
+    }
+}
