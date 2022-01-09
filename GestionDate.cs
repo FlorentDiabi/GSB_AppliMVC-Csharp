b35d3c2c4f068599d6c0fa588be39f3b34f40e40
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2022-01-09 12:04:59 +0100

    Ajout classe de gestion date

---
+++ GestionDate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSB_AppliMVC_Csharp
+{
+    public abstract class GestionDate
+    {
+        public static string GetMoisPrecedent()
+        {
+            return GetMoisPrecedent(DateTime.Now);
+        }
+
+        public static string GetMoisPrecedent(DateTime date)
+        {
+            int month = (date.Month == 1) ? 12 : date.Month - 1;
+            return (Convert.ToString(month).Length == 1) ? "0" + Convert.ToString(month) : Convert.ToString(month);
+        }
+
+        public static string GetMoisSuivant()
+        {
+            return GetMoisSuivant(DateTime.Now);
+        }
+
+        public static string GetMoisSuivant(DateTime date)
+        {
+            int month = (date.Month == 12) ? 1 : date.Month + 1;
+            return (Convert.ToString(month).Length == 1) ? "0" + Convert.ToString(month) : Convert.ToString(month);
+        }
+
+        public static bool Entre(int jourDebut, int jourFin)
+        {
+            return Entre(jourDebut, jourFin, DateTime.Now);
+        }
+
+        public static bool Entre(int jourDebut, int jourFin, DateTime date)
+        {
+            return date.Day >= jourDebut && date.Day <= jourFin;
+        }
+
+    }
+}
