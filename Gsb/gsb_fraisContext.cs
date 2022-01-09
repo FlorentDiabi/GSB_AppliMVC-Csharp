dfad7c839e53d0b090e2929a9970cd1530202b6c
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2022-01-09 12:36:45 +0100

    No comments (header)

--- Gsb/gsb_fraisContext.cs
+++ Gsb/gsb_fraisContext.cs
@@ -3,10 +3,6 @@ using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
-
 namespace GSB_AppliMVC_Csharp.Gsb
 {
     public partial class gsb_fraisContext : DbContext
